38e9b0efaf8e5aea4093abdcb69aecf7f1bb1bd9
Alvin Jason <alvin.kamau1@student.moringaschool.com>
2026-03-17 11:35:21 +0300

    Initial Vite Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-red-600 text-center justify-between font-extrabold underline '>Plate By Shiru</h1>
+    </div>
+  )
+}
+
+export default App
