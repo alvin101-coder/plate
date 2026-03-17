e40da2c97fcfd094097783e3a1bab62f1618d681
Alvin Jason <alvin.kamau1@student.moringaschool.com>
2026-03-17 16:42:48 +0300

    Add MenuDisplay with category filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Service from './Components/Service'
+import MenuDisplay from './Components/MenuDisplay'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Service />
+      <MenuDisplay />
     </div>
   )
 }
