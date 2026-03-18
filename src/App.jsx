76a8800ca046885866b9a0473f9573538e0e5b5c
Alvin Jason <alvin.kamau1@student.moringaschool.com>
2026-03-18 11:12:57 +0300

    add Footer component with newsletter subscription, social icons, navigation links, and copyright section

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Hero from './Components/Hero'
 import Service from './Components/Service'
 import MenuDisplay from './Components/MenuDisplay'
 import ReservationForm from './Components/ReservationForm'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Service />
       <MenuDisplay />
       <ReservationForm />
+      <Footer />
     </div>
   )
 }
